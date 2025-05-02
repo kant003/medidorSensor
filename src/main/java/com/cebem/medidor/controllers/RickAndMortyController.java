f85448531935b49c506758ea0bce87c467a0a218
kant003 <gonzalezm.angel@gmail.com>
2025-05-02 12:17:19 +0200

    uso de servicio

--- src/main/java/com/cebem/medidor/controllers/RickAndMortyController.java
+++ src/main/java/com/cebem/medidor/controllers/RickAndMortyController.java
@@ -6,25 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.client.RestTemplate;
 
 import com.cebem.medidor.models.RickandmortyCharacter;
+import com.cebem.medidor.services.RickandmortyService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class RickAndMortyController {
 
-    private final RestTemplate restTemplate;
-
-    public RickAndMortyController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    //private final RestTemplate restTemplate;
+    private final RickandmortyService rickandmortyService;
+   
 
     @GetMapping("/rickandmortyweb")
     public String getRandomCharacter(Model model) {
-        // Hacer una solicitud a la API externa para obtener un personaje aleatorio
-        String apiUrl = "https://rickandmortyapi.com/api/character/";
-        RickandmortyCharacter character = restTemplate.getForObject(apiUrl + "1", RickandmortyCharacter.class); // Aquí puedes usar otro endpoint para obtener un random
-
+        RickandmortyCharacter character = rickandmortyService.getCharacterRandom();
         // Agregar el personaje al modelo de Thymeleaf
         model.addAttribute("character", character);
-
         // Devolver el nombre del template
         return "characterCard";
     }
